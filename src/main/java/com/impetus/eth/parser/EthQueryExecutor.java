5d4ff3d73045fe73faadb6c181fa3e0f2de7ac4e
karthikp.manchala <karthikp.manchala@impetus.co.in>
2018-04-06 13:58:44 +0530

    removed unused imports

--- src/main/java/com/impetus/eth/parser/EthQueryExecutor.java
+++ src/main/java/com/impetus/eth/parser/EthQueryExecutor.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
