a3caba10742e10d4b0b8a94308926fe8e0e74aad
Ashish Kumar Shukla <ashishk.shukla@impetus.co.in>
2018-05-08 17:59:33 +0530

    skip mvn deploy for examples

--- eth-jdbc-examples/src/main/java/com/impetus/blkchn/eth/Query.java
+++ eth-jdbc-examples/src/main/java/com/impetus/blkchn/eth/Query.java
@@ -7,22 +7,19 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Query 
-{
-    public static void main(String[] args) throws ClassNotFoundException
-    {
-        
+public class Query {
+    public static void main(String[] args) throws ClassNotFoundException {
+
         String url = "jdbc:blkchn:ethereum://172.25.41.52:8545";
         String driverClass = "com.impetus.eth.jdbc.EthDriver";
-        try
-        {
+        try {
             Class.forName(driverClass);
 
             Connection conn = DriverManager.getConnection(url, null);
             Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("select count(*) as cnt, blocknumber from transaction where blocknumber > 1652349 and blocknumber < 1652354 group by blocknumber");
-            while (rs.next())
-            {
+            ResultSet rs = stmt.executeQuery(
+                    "select count(*) as cnt, blocknumber from transaction where blocknumber > 1652349 and blocknumber < 1652354 group by blocknumber");
+            while (rs.next()) {
                 System.out.print("" + rs.getInt(1) + "   |   ");
                 System.out.print("" + rs.getString("blocknumber"));
                 System.out.println();
@@ -34,9 +31,7 @@ public class Query
             System.out.println("column label name : " + rsMetaData.getColumnLabel(1));
             System.out.println("column Name : " + rsMetaData.getColumnName(1));
             System.out.println("tableName : " + rsMetaData.getTableName(1));
-        }
-        catch (SQLException e1)
-        {
+        } catch (SQLException e1) {
             e1.printStackTrace();
         }
 
