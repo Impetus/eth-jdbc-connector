48e6c36914495972f3d15f8b53f9b26fe099c2d4
Ashish Kumar Shukla <ashishk.shukla@impetus.co.in>
2018-07-19 12:00:47 +0530

    1.added setBoolean and some other methods in prepared statement 2.added test cases 3.added examples for prepared statement in examples module

--- eth-jdbc-driver/src/test/java/com/impetus/eth/integration/test/TestInsertPreparedStatement.java
+++ eth-jdbc-driver/src/test/java/com/impetus/eth/integration/test/TestInsertPreparedStatement.java
@@ -13,53 +13,58 @@
 * * See the License for the specific language governing permissions and
 * * limitations under the License.
 ******************************************************************************/
-package com.impetus.blkchn.eth;
+package com.impetus.eth.integration.test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.Properties;
 
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.impetus.blkch.BlkchnException;
 import com.impetus.eth.jdbc.DriverConstants;
+import com.impetus.eth.test.util.ConnectionUtil;
+import com.impetus.test.catagory.IntegrationTest;
 
-public class InsertPreparedStatement {
-    private static final Logger LOGGER = LoggerFactory.getLogger(InsertPreparedStatement.class);
+@Category(IntegrationTest.class)
+public class TestInsertPreparedStatement {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestAlias.class);
 
-    public static void main(String[] args) throws ClassNotFoundException {
+    @Test
+    public void testInsertQuery() {
 
-        String url = "jdbc:blkchn:ethereum://ropsten.infura.io/1234";
+        String url = ConnectionUtil.getEthUrl();
         String driverClass = "com.impetus.eth.jdbc.EthDriver";
         try {
             Class.forName(driverClass);
             Properties prop = new Properties();
             prop.put(DriverConstants.KEYSTORE_PATH,
-                    "/local/path/to/ethereum/keystore/UTC--XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-            prop.put(DriverConstants.KEYSTORE_PASSWORD, "<password>");
+                    "D:\\Ethereum\\Ashish\\UTC--2017-09-11T04-49-35.811622052Z--a76cd046cf6089fe2adcf1680fcede500e44bacd");
+            prop.put(DriverConstants.KEYSTORE_PASSWORD, "impetus123");
             Connection conn = DriverManager.getConnection(url, prop);
             String query = "insert into transaction (toAddress, value, unit, async) values (?, ?, 'ether', true)";
 
             PreparedStatement stmt = conn.prepareStatement(query);
-            
+            stmt.setObject(2, 0.0001);
             stmt.setObject(1, "0x8144c67b144A408ABC989728e32965EDf37Adaa1");
-            stmt.setObject(2, 0.001);
+            int i = stmt.executeUpdate();
+
+            stmt.setObject(2, 0.0002);
+            stmt.setObject(1, "0x8144c67b144A408ABC989728e32965EDf37Adaa1");
             stmt.executeUpdate();
 
-            stmt.setObject(1, "0x8144c67b144A408ABC989728e32965EDf37Adaa1");
-            stmt.setObject(2, 0.002);
-            stmt.executeUpdate();
-
-            conn.close();
-            stmt.close();
+            LOGGER.info(" status " + i);
             LOGGER.info("Connection closed " + conn.isClosed());
             LOGGER.info("Statement closed " + stmt.isClosed());
 
-        } catch (SQLException e1) {
-            e1.printStackTrace();
+        } catch (Exception e) {
+            throw new BlkchnException(e.getMessage());
         }
 
     }
+
 }
---
+++ eth-jdbc-driver/src/test/java/com/impetus/eth/integration/test/TestQueryPreparedStatement.java
@@ -0,0 +1,94 @@
+/******************************************************************************* 
+* * Copyright 2018 Impetus Infotech.
+* *
+* * Licensed under the Apache License, Version 2.0 (the "License");
+* * you may not use this file except in compliance with the License.
+* * You may obtain a copy of the License at
+* *
+* * http://www.apache.org/licenses/LICENSE-2.0
+* *
+* * Unless required by applicable law or agreed to in writing, software
+* * distributed under the License is distributed on an "AS IS" BASIS,
+* * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* * See the License for the specific language governing permissions and
+* * limitations under the License.
+******************************************************************************/
+package com.impetus.eth.integration.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.impetus.blkch.BlkchnException;
+import com.impetus.eth.test.util.ConnectionUtil;
+import com.impetus.test.catagory.IntegrationTest;
+
+@Category(IntegrationTest.class)
+public class TestQueryPreparedStatement {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestAlias.class);
+
+    @Test
+    public void testQuery() {
+
+        String url = ConnectionUtil.getEthUrl();
+        String driverClass = "com.impetus.eth.jdbc.EthDriver";
+        try {
+            Class.forName(driverClass);
+            Connection conn = DriverManager.getConnection(url, null);
+            PreparedStatement stmt = conn.prepareStatement(
+                    "select count(*) as cnt, blocknumber from transaction where blocknumber = 1234 or blocknumber = ? or blocknumber =12323 or blocknumber = ? group by blocknumber");
+            stmt.setObject(2, 2222486);
+            stmt.setObject(1, 2222487);
+
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                LOGGER.info("" + rs.getInt(1));
+                LOGGER.info("" + rs.getString("blocknumber"));
+            }
+            LOGGER.info("Result set MetaData");
+
+            ResultSetMetaData rsMetaData = rs.getMetaData();
+            LOGGER.info("Total Columns : " + rsMetaData.getColumnCount());
+            LOGGER.info("column label name : " + rsMetaData.getColumnLabel(1));
+            LOGGER.info("column Name : " + rsMetaData.getColumnName(1));
+            LOGGER.info("tableName : " + rsMetaData.getTableName(1));
+
+            stmt.setObject(2, 2222488);
+            stmt.setObject(1, 2222489);
+
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                LOGGER.info("" + rs.getInt(1));
+                LOGGER.info("" + rs.getString("blocknumber"));
+            }
+            LOGGER.info("Result set MetaData");
+
+            rsMetaData = rs.getMetaData();
+            LOGGER.info("Total Columns : " + rsMetaData.getColumnCount());
+            LOGGER.info("column label name : " + rsMetaData.getColumnLabel(1));
+            LOGGER.info("column Name : " + rsMetaData.getColumnName(1));
+            LOGGER.info("tableName : " + rsMetaData.getTableName(1));
+
+            conn.close();
+            rs.close();
+            stmt.close();
+            LOGGER.info("Connection closed " + conn.isClosed());
+            LOGGER.info("ResultSet closed " + rs.isClosed());
+            LOGGER.info("Statement closed " + stmt.isClosed());
+
+        } catch (Exception e) {
+            throw new BlkchnException(e.getMessage());
+        }
+
+    }
+
+}
