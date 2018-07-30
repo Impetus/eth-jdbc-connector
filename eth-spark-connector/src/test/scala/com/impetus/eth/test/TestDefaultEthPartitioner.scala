0d4e8d26f5582bc6b5e1a08fef38fb22c2096c70
ayush.vasu <ayush.vasu@impetus.co.in>
2018-07-30 17:43:29 +0530

    Eth Spark connector test cases

---
+++ eth-spark-connector/src/test/scala/com/impetus/eth/test/TestDefaultEthPartitioner.scala
@@ -0,0 +1,41 @@
+package com.impetus.eth.test
+
+import java.math.BigInteger
+import com.impetus.blkch.spark.connector.rdd.ReadConf
+import com.impetus.blkch.spark.connector.BlkchnConnector
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.eth.EthConnectorConf
+import org.scalatest.{BeforeAndAfter, FlatSpec}
+import com.impetus.eth.spark.connector.rdd.partitioner.DefaultEthPartitioner
+
+
+class TestDefaultEthPartitioner extends FlatSpec with BeforeAndAfter {
+
+  var spark: SparkSession = null
+  var ethConnectorConf : EthConnectorConf = null
+  var blkchnConnector : BlkchnConnector = null
+  var defaultPartition : DefaultEthPartitioner = null
+
+  before {
+    spark = SparkSession.builder().master("local").appName("Test").getOrCreate()
+    Class.forName("com.impetus.eth.jdbc.EthDriver")
+    ethConnectorConf = EthConnectorConf(spark.sparkContext.getConf,Map("url" -> "jdbc:blkchn:ethereum://ropsten.infura.io/1234"))
+    blkchnConnector = new BlkchnConnector(ethConnectorConf)
+    defaultPartition = DefaultEthPartitioner
+  }
+
+  "Default Eth Partition" should "give same number of partition as specified" in {
+    val readConf = ReadConf(Some(3), None, "Select * FROM block")(defaultPartition)
+    val bufferOfRange = defaultPartition.getPartitions(blkchnConnector,readConf)
+    assert(bufferOfRange.size == 3)
+  }
+
+  it should "give same number of line in partition if specified fetchSizeInRows" in {
+    val readConf = ReadConf(None, Some(3000), "Select * FROM block")(defaultPartition)
+    val bufferOfRange = defaultPartition.getPartitions(blkchnConnector,readConf)
+    val max = bufferOfRange(0).range.getRangeList.getRanges.get(0).getMax.asInstanceOf[BigInteger]
+    val min = bufferOfRange(0).range.getRangeList.getRanges.get(0).getMin.asInstanceOf[BigInteger]
+    assert(max.subtract(min).intValue() + 1 == 3000)
+  }
+
+}
---
+++ eth-spark-connector/src/test/scala/com/impetus/eth/test/TestFirst.scala
@@ -0,0 +1,48 @@
+package com.impetus.eth.test
+
+import com.impetus.blkch.spark.connector.rdd.{BlkchnRDD, ReadConf}
+import com.impetus.blkch.spark.connector.rdd.partitioner.BlkchnPartitioner
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.eth.EthSpark
+import org.scalatest.{BeforeAndAfter, FlatSpec}
+
+class TestFirst extends FlatSpec with BeforeAndAfter{
+
+  var spark: SparkSession = null
+  val ethPartitioner:BlkchnPartitioner = TestEthPartitioner
+  var readConf:ReadConf = null
+  var rdd: BlkchnRDD[Row] = null
+
+  before {
+    spark = SparkSession.builder().master("local").appName("Test").getOrCreate()
+    readConf = ReadConf(Some(3), None, "Select * FROM block")(ethPartitioner)
+    rdd = EthSpark.load[Row](spark.sparkContext, readConf,Map("url" -> "jdbc:blkchn:ethereum://ropsten.infura.io/1234"))
+    Class.forName("com.impetus.eth.jdbc.EthDriver")
+  }
+
+  "Eth Spark" should "have Read Conf" in {
+    assert(readConf != null)
+  }
+
+  it should "have rdd with same partition as specified" in {
+    assert(rdd.getNumPartitions == 3)
+  }
+
+  it should "have not empty rdd" in{
+    assert(rdd.collect().isEmpty != true)
+  }
+
+  it should "be able to create data frame" in {
+    val option = readConf.asOptions() ++ Map("url" -> "jdbc:blkchn:ethereum://172.25.41.52:8545")
+    val df = spark.read.format("org.apache.spark.sql.eth").options(option).load()
+    assert(df.select(df.col("blocknumber")).collect().length > 0)
+  }
+
+  after {
+    spark.stop()
+  }
+
+
+
+
+}
